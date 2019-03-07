91853e5bb127c42da0ab373fcb1f37c268dfcaa5
feynman911 <feynman911@gmail.com>
2019-03-07 22:45:43 +0900

    プロジェクト ファイルを追加します。

---
+++ OxySample/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using OxySample.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using System.Windows;
+
+namespace OxySample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
